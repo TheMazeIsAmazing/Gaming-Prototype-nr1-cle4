eaf51de22c238c2888fb08da51870e83020b44b8
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-06-22 11:32:18 +0200

    Update ricky.ts

--- src/ricky.ts
+++ src/ricky.ts
@@ -1,14 +1,19 @@
+//import pixi
 import * as PIXI from 'pixi.js'
+
+//import classes
 import { movingObject } from "./movingObject"
 
 export class Ricky extends movingObject {
 
     constructor(x: number, y: number, texture: PIXI.Texture, game : PIXI.Application) {
+        //give desired items to the movingObject class
         super(x, y, texture, game)
         this.anchor.set(0.5);
     }
 
     update(delta: number) {
+        //this code makes Ricky rotate 10 times
         if (this.angle < 3600) {
             this.angle +=  delta
         }
