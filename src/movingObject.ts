fff7bb6d8f57efbb1b800cede6cc4cb00c8eb508
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-06-22 11:35:18 +0200

    Update movingObject.ts

--- src/movingObject.ts
+++ src/movingObject.ts
@@ -1,16 +1,23 @@
+//import pixi
 import * as PIXI from 'pixi.js'
 
 export class movingObject extends PIXI.Sprite {
     private speed:number
 
     constructor(x: number, y: number, texture: PIXI.Texture, game : PIXI.Application) {
+        //give texture to PIXI.Sprite
         super(texture)
+        
+        //set the x, y and speedvalues
         this.x = x
         this.y = y
-        game.stage.addChild(this)
         this.speed = (Math.random() * 1) + 0.5
+        
+        //add this object to the game
+        game.stage.addChild(this)
     }
-
+    
+    //this function gets ans returns speed
     getSpeed () {
         return this.speed;
     }
