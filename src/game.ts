a50d36b81c26a86e993c47b6328419aa612c21c9
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-06-22 11:13:43 +0200

    Update game.ts

--- src/game.ts
+++ src/game.ts
@@ -1,18 +1,22 @@
+//import pixi
 import * as PIXI from 'pixi.js'
+
+//import images & music
 import fishImage from "./images/fish.png"
 import bubbleImage from "./images/bubble.png"
 import waterImage from "./images/water.jpg"
-import rickyImage from ".
-/images/ricky.png"
+import rickyImage from "./images/ricky.png"
+import backgroundTrack from "url:./music/backgroundaudio.mp3" 
+
+//import classes
 import { Fish } from './fish'
 import { Bubble } from './bubble'
 import { Ricky } from './ricky'
-
-
-import backgroundTrack from "url:./music/backgroundaudio.mp3"  
-
+ 
+//if window resizes, do function resizeHandler
 window.addEventListener('resize', resizeHandler)
 
+//this code changes the height and width to the current values
 function resizeHandler() {
     height = window.innerHeight
     width = window.innerWidth 
@@ -20,6 +24,7 @@ function resizeHandler() {
     game.pixi.view.width = height
 }
 
+//global variables
 let height = window.innerHeight
 let width = window.innerWidth
 let howManyFish = 1
@@ -37,34 +42,46 @@ export class Game {
         document.body.appendChild(this.pixi.view)
         let viewWidth = (this.pixi.renderer.width / this.pixi.renderer.resolution);
 
+        //load all textures
         this.pixi.loader
             .add("fishTexture", fishImage)
             .add("waterTexture", waterImage)
             .add('bubbleTexture', bubbleImage)
             .add('rickyTexture', rickyImage)
             .add("bgMusic", backgroundTrack)
+        
+        // after loader is done, load doneLoading function
         this.pixi.loader.load(() => this.doneLoading(viewWidth))
+        
+        // Add ticker to pixi and load update function
         this.pixi.ticker.add((delta) => this.update())
     }
 
     doneLoading(viewWidth:number) {
+        //make sure the bg track is loaded before anything else because browser delay
         let bgMusic = this.pixi.loader.resources["bgMusic"].data!
         bgMusic.play()
-
+        
+        //load background image and add it to game
         let water = new PIXI.Sprite(this.pixi.loader.resources["waterTexture"].texture!)
         water.scale.x = 512 / viewWidth;
         water.scale.y = water.scale.x;
         this.pixi.stage.addChild(water)
+        
+        //this code adds the desired amount of fish and bubbles to the game
         for (let i = 1; i <= howManyFish; i++) {
             this.fishes.push(new Fish((Math.random() * width), (Math.random() * height), this.pixi.loader.resources["fishTexture"].texture!, game.pixi))
         }
         for (let i = 1; i <= howManyBubbles; i++) {
             this.bubbles.push(new Bubble((Math.random() * width), (Math.random() * height), this.pixi.loader.resources["bubbleTexture"].texture!, game.pixi))
         }
+        
+        //this code adds ricky to the game
         this.ricky.push(new Ricky((Math.random() * width), (Math.random() * height), this.pixi.loader.resources["rickyTexture"].texture!, game.pixi))
     }
 
     update() {
+        //for each instance of fish/bubble/ricky within the assosiated array execute the update function
         for (let fishje of this.fishes) {
             fishje.update(fishje.getSpeed(), height, width)
         }
