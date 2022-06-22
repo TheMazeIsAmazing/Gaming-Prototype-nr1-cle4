477cdf4955b350823aaa35b8a69549c127cecaf7
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2022-06-22 11:30:43 +0200

    Update fish.ts

--- src/fish.ts
+++ src/fish.ts
@@ -1,22 +1,31 @@
+//import pixi
 import * as PIXI from 'pixi.js'
+
+//import classes
 import { movingObject } from "./movingObject"
 
 export class Fish extends movingObject {
 
     constructor(x: number, y: number, texture: PIXI.Texture, game : PIXI.Application) {
+        //give desired items to the movingObject class
         super(x, y, texture, game)
     }
 
     update(delta: number, height: number, width: number ) {
+        //checks if the bubble has left the screen horizontally. If that's the case place it at the right
         if (this.x < -200) {
             this.x = width + (Math.random() * width)
         }
+        
+        //checks if the bubble has left the screen vertically. If that's the case place it at at a random y interval
         if (this.y < -100) {
             this.y = height + (Math.random() * height)
         }
         if (this.y > height + 100) {
             this.y = height - (Math.random() * height)
         }
+                
+        //this code makes the fish move both sideways and up and down
         this.x -= delta * 1.2
         this.y += Math.sin(this.x * 0.02) * delta
     }
